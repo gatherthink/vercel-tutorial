3970d083acdb397e9a80d6e8405a08c14a1cc800
gatherthink <gatherthink@gmail.com>
2022-11-25 18:05:17 -0500

    updates
    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+
+
+
+
+<script>
+    let quantity = 0;
+
+    function addToCart() {
+        quantity = ++quantity
+    }
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<h2>There are {quantity} in the shopping cart</h2>
+
+<button on:click={addToCart}>
+    Add To cart
+</button>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
